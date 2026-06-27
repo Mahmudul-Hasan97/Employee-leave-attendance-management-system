359238aecb19db3aa89dd5b5cc3dc4dda8b58aea
Mahmudul-Hasan97 <mhasantushar97@gmail.com>
2026-06-28 04:09:33 +0600

    Added sample data

---
+++ docs/diagrams/database/sample-data.sql
@@ -0,0 +1,10 @@
+INSERT INTO departments(department_name)
+VALUES
+('HR'),
+('IT'),
+('Finance');
+
+INSERT INTO employees(first_name,last_name,email,phone)
+VALUES
+('Rahim','Ahmed','rahim@gmail.com','01711111111'),
+('Karim','Hasan','karim@gmail.com','01822222222');
